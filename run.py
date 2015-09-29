9662e5601e123444de009ca205101aca2fc38a4b
Lawrence Hoffman <ljhffmn@gmail.com>
2015-09-28 20:41:11 -0600

    Added some basic argument parsing

--- run.py
+++ run.py
@@ -1,5 +1,15 @@
 #!flask/bin/python
-from app import app 
+from app import app
+import sys
+import argparse
+
 
 if __name__ == "__main__":
-    app.run(debug=True, threaded=False)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p','--port', type=int, help='Set port to use')
+    parser.add_argument('-d','--debug', action='store_true', help='Turn Debugging on')
+    parser.add_argument('-t','--no_threads', action='store_false', help='Turn off threading')
+    args = parser.parse_args()
+
+    app.run(debug=args.debug, threaded=args.no_threads, port=args.port)
