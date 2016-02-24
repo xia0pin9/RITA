537585b28596687ac1ce3d05448c93fe6bc61c79
Lawrence Hoffman <ljhffmn@gmail.com>
2016-02-23 20:38:43 -0700

    Added a host option

--- run.py
+++ run.py
@@ -8,8 +8,9 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-p','--port', type=int, help='Set port to use')
+    parser.add_argument('-i','--host', help='Set host to run on')
     parser.add_argument('-d','--debug', action='store_true', help='Turn Debugging on')
     parser.add_argument('-t','--no_threads', action='store_false', help='Turn off threading')
     args = parser.parse_args()
 
-    app.run(debug=args.debug, threaded=args.no_threads, port=args.port)
+    app.run(debug=args.debug, threaded=args.no_threads, port=args.port, host=args.host)
